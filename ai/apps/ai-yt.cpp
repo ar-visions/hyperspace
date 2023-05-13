fd0c4729704d6d5b944c08e7b8054f26882f0d5c
AR Visions <kalen@krworks.com>
2023-05-12 23:31:24 -0700

    changes to test app

--- ai/apps/ai-yt.cpp
+++ ai/apps/ai-yt.cpp
@@ -1,33 +1,26 @@
 #include <core/core.hpp>
 #include <async/async.hpp>
 #include <net/net.hpp>
+#include <math/math.hpp>
+#include <image/image.hpp>
 
 using namespace ion;
-
 /// could train lots of things just from national geographic-like videos
 /// how animals move in space relative to other animals.
 /// thats interesting because you can certainly map lots of information
 /// ----------------------------
 int main() {
-    lambda<memory*(memory*)> mtest;
-    int test = 1;
-    ///
     str  api_key     = "AIzaSyAg4nh93xKESkGZvv7Ocv2PBBFAM1jyDSs";
     str  channel_id  = "UCpVm7bg6pXKo1Pr6k5kxG9A";
     uri  request_url = fmt { "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={0}&maxResults=1&key={1}", { channel_id, api_key }};
     ///
-    request(request_url, null).then([](mx result) {
-        message::members &rs_0 = result.ref<message::members>();
-        //struct members {
-        //    uri     query;
-        //    mx      code = int(0);
-        //    map<mx> headers;
-        //    mx      content; /// best to store as mx, so we can convert directly in lambda arg, functionally its nice to have delim access in var.
-        //} &m;
-        console.log("code: {0}", {rs_0.code, rs_0.headers});
-        printf("abc\n");
+    request(request_url, null).then([](mx res) {
+        message msg { res.grab() };
+        var content = msg->content;
+        console.log("item 0 etag: {0}", { content["items"][0]["etag"] });
+    }).except([](mx err) {
+        console.error("http request error: {0}", { err });
     });
-    
     ///
     return async::await();
 }
