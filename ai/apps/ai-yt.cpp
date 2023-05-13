17cb82acf9770e89e60703cec31ba9d1dcece102
AR Visions <kalen@krworks.com>
2023-05-13 09:36:21 -0700

    Update ai-yt.cpp

--- ai/apps/ai-yt.cpp
+++ ai/apps/ai-yt.cpp
@@ -5,15 +5,17 @@
 #include <image/image.hpp>
 
 using namespace ion;
-/// could train lots of things just from national geographic-like videos
-/// how animals move in space relative to other animals.
-/// thats interesting because you can certainly map lots of information
-/// ----------------------------
+
+/// ------------------------------------------------------------
+/// audio annotation app first
+/// ------------------------------------------------------------
+
 int main() {
     str  api_key     = "AIzaSyAg4nh93xKESkGZvv7Ocv2PBBFAM1jyDSs";
     str  channel_id  = "UCpVm7bg6pXKo1Pr6k5kxG9A";
     uri  request_url = fmt { "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={0}&maxResults=1&key={1}", { channel_id, api_key }};
     ///
+    /// switch to vulkan display, get working now.  json from https works
     request(request_url, null).then([](mx res) {
         message msg { res.grab() };
         var content = msg->content;
